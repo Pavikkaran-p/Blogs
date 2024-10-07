42322ad75bb95918f84262c8cda8c1be32585d59
Pavikkaran <142660254+Pavikkaran-p@users.noreply.github.com>
2024-10-07 10:51:36 +0530

    components added

---
+++ Frontend/src/pages/SignUp.tsx
@@ -0,0 +1,17 @@
+import Quote from "../components/Quote"
+
+const SignUp = () => {
+  return (
+    <>
+    <div>SignUp</div>
+    <div className="grid grid-cols-2 invisible lg:visible">
+      <div>
+        
+      </div>
+      <Quote/>
+    </div>
+    </>
+  )
+}
+
+export default SignUp
